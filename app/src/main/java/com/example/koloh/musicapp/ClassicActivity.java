f92b22bab58e4f9b8687f4402307282abb008e40
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-04-10 23:37:45 +0200

    array of song list created in the classic category Activity

--- app/src/main/java/com/example/koloh/musicapp/ClassicActivity.java
+++ app/src/main/java/com/example/koloh/musicapp/ClassicActivity.java
@@ -9,5 +9,10 @@ public class ClassicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_classic );
+
+        // Creating an array of classic songs called classic.
+        String[] classic = new String[2];
+        classic[0] = "Start Me Up";
+        classic[1] = "Sweet Home Alabama";
     }
 }
